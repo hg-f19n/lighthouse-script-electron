739e6210c2914979cc2a9a7694b5d7fc0aed8aec
hg-f19n <hg@f19n.com>
2024-06-13 17:20:57 +0200

    Revert "paths and readme updatye"
    This reverts commit c6a5c5dc6fd0b12da9d10e654662387e285d46d5.

--- lighthouse-script.js
+++ lighthouse-script.js
@@ -15,15 +15,13 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const userHome = app.getPath('home');
 
-const appDir = path.join(userHome, 'Lighthouse');
-
 //const inputFile = 'urls.txt';
 const inputFile = path.join(__dirname, 'urls.txt');
 console.log(inputFile);
 //const logDir = 'logs';
-const logDir = path.join(appDir, 'logs');
+const logDir = path.join(userHome, 'YourAppName', 'app-1.log');
 //const resultsDir = 'results';
-const resultsDir = path.join(appDir, 'results');
+const resultsDir = path.join(userHome, 'results');
 
 /*const getLighthouseVersion = async () => {
   const lighthousePkg = await import('lighthouse/package.json', { assert: { type: 'json' } });
